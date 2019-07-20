5de952fd878be88112a85df019afc1ac210988bd
SodaCode <dominicsaechao1@gmail.com>
2019-07-20 10:58:46 -0700

    added routes and made the partial bones

--- app.js
+++ app.js
@@ -1,11 +1,51 @@
-const express = require("express");
+const express = require("express"),
+  app = express(),
+  flash = require("connect-flash"),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  methodOverride = require("method-override"),
+  LocalStrategy = require("passport-local"),
+  User = require("./models/users");
 
 const app = express();
 
+mongoose
+  .connect(process.env.DATABASEURL, () => {
+    console.log("Connected to DB");
+  })
+  .catch(err => {
+    console.log("Error:" + err.message);
+  });
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
 
 app.get("/", (req, res) => {
   res.render("landing");
+});
+
+app.get("/catalogue", (req, res) => {
+  res.render("catalogue");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("cart");
+});
+
+app.get("/checkout", (req, res) => {
+  res.render("checkout");
 });
 
 app.listen(3000, () => {
